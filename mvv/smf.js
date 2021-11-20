a14d1ae52abae3d8fcdc2ded7ec0ef8f8eacc2c3
Makoto Onuki <makoto.onuki@gmail.com>
2021-11-19 21:20:40 -0800

    BytesWriter done

--- mvv/smf.js
+++ mvv/smf.js
@@ -1,3 +1,6 @@
+// SMF Format: https://ccrma.stanford.edu/~craig/14q/midifile/MidiFileFormat.html
+// https://www.music.mcgill.ca/~gary/306/week9/smf.html
+
 class BytesWriter {
     #cap = 8; // 1024 * 32;
     #size = 0;
@@ -7,20 +10,84 @@ class BytesWriter {
         this.#buf = new Uint8Array(this.#cap);
     }
 
-    writeByte(b) {
-        this.#ensureSize(1);
-        this.#buf[this.#size] = b;
-        this.#size++;
+    writeVar(val) {
+        if (val >= 0x200000) {
+            var v = val / 0x200000;
+            val &= (0x200000 - 1)
+            this.writeI8(0x80 | v);
+        }
+        if (val >= 0x4000) {
+            var v = val / 0x4000;
+            val &= (0x4000 - 1)
+            this.writeI8(0x80 | v);
+        }
+        if (val >= 0x80) {
+            var v = val / 0x80;
+            val &= (0x80 - 1)
+            this.writeI8(0x80 | v);
+        }
+        this.writeI8(val);
+        return this;
     }
 
-    #ensureSize(size) {
-        if (this.#cap >= (this.#size + size)) {
-            return;
-        }
+    writeI8(val) {
+        this.setI8(this.#size, val);
+        this.#size += 1;
+        return this;
+    }
+
+    writeI16(val) {
+        this.setI16(this.#size, val);
+        this.#size += 2;
+        return this;
+    }
+
+    writeI32(val) {
+        this.setI32(this.#size, val);
+        this.#size += 4;
+        return this;
+    }
+
+    setI8(pos, val) {
+        this.#ensureCap(pos);
+        this.#buf[pos + 0] = (val >>  0) & 255;
+        return this;
+    }
+
+    setI16(pos, val) {
+        this.#ensureCap(pos + 1);
+        this.#buf[pos + 0] = (val >>  8) & 255;
+        this.#buf[pos + 1] = (val >>  0) & 255;
+        return this;
+    }
+
+    setI32(pos, val) {
+        this.#ensureCap(pos + 3);
+        this.#buf[pos + 0] = (val >> 24) & 255;
+        this.#buf[pos + 1] = (val >> 16) & 255;
+        this.#buf[pos + 2] = (val >>  8) & 255;
+        this.#buf[pos + 3] = (val >>  0) & 255;
+        return this;
+    }
+
+    #grow() {
         this.#cap *= 2;
         var nb = new Uint8Array(this.#cap);
         nb.set(this.#buf);
         this.#buf = nb;
+        return this;
+    }
+
+    #ensureCap(cap) {
+        if (this.#cap >= cap) {
+            return;
+        }
+        this.#grow();
+        return this;
+    }
+
+    #ensureGrowth(size) {
+        return this.#ensureCap(this.#size + size);
     }
 
     getSize() {
